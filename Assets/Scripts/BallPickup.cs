6a27412ba215b8814d7c7a3eb86029b20884137b
Amira Medhat <amirafarag837@gmail.com>
2026-06-21 15:37:30 +0300

    Fix ball freeze at start

--- Assets/Scripts/BallPickup.cs
+++ Assets/Scripts/BallPickup.cs
@@ -19,43 +19,46 @@ public class BallPickup : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    rb.isKinematic = true; // freeze ball at start
+        rb.isKinematic = true; // freeze ball at start
+        rb.constraints = RigidbodyConstraints.FreezeAll;
     }
 
     void Update()
+{
+    float distanceToBall = Vector3.Distance(palmPoint.transform.position, transform.position);
+
+    if (gripDetection.isGripping && distanceToBall < pickupDistance && !isHeld)
     {
-        float distanceToBall = Vector3.Distance(palmPoint.transform.position, transform.position);
-
-        if (gripDetection.isGripping && distanceToBall < pickupDistance && !isHeld)
-        {
-            isHeld = true;
-            rb.isKinematic = true;
-        }
-
-        if (isHeld)
-        {
-            transform.position = palmPoint.transform.position;
-            throwVelocity = (palmPoint.transform.position - lastPalmPosition) / Time.deltaTime;
-        }
-
-        if (wasGripping && !gripDetection.isGripping && isHeld)
-        {
-            isHeld = false;
-            rb.isKinematic = false;
-            lockedZ = transform.position.z;
-            float speed = Mathf.Max(Mathf.Abs(throwVelocity.x), 15f);
-            rb.linearVelocity = new Vector3(speed + 10f, 0, 0);
-            ballThrown = true;
-            cameraController.StartFollowing();
-        }
-
-        if (ballThrown)
-        {
-            Vector3 pos = rb.position;
-            rb.MovePosition(new Vector3(pos.x, pos.y, lockedZ));
-        }
-
-        wasGripping = gripDetection.isGripping;
-        lastPalmPosition = palmPoint.transform.position;
+        isHeld = true;
+        rb.constraints = RigidbodyConstraints.None;
+        rb.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        rb.isKinematic = true;
     }
+
+    if (isHeld)
+    {
+        transform.position = palmPoint.transform.position;
+        throwVelocity = (palmPoint.transform.position - lastPalmPosition) / Time.deltaTime;
+    }
+
+    if (wasGripping && !gripDetection.isGripping && isHeld)
+    {
+        isHeld = false;
+        rb.isKinematic = false;
+        lockedZ = transform.position.z;
+        float speed = Mathf.Max(Mathf.Abs(throwVelocity.x), 15f);
+        rb.linearVelocity = new Vector3(speed + 10f, 0, 0);
+        ballThrown = true;
+        cameraController.StartFollowing();
+    }
+
+    if (ballThrown)
+    {
+        Vector3 pos = rb.position;
+        rb.MovePosition(new Vector3(pos.x, pos.y, lockedZ));
+    }
+
+    wasGripping = gripDetection.isGripping;
+    lastPalmPosition = palmPoint.transform.position;
+}
 }
